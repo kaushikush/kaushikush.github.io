5238cd31a5245db778579cf748525a07483308b2
Kaushik <physicskush@gmail.com>
2024-12-24 22:19:43 +0530

    Set up emailjs in the contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,9 +39,9 @@ $(document).ready(function () {
 
     // <!-- emailjs to mail contact form data -->
     $("#contact-form").submit(function (event) {
-        emailjs.init("user_TTDmetQLYgWCLzHTDgqxm");
+        emailjs.init("fT6wj3c4RyPfE7IaD");
 
-        emailjs.sendForm('contact_service', 'template_contact', '#contact-form')
+        emailjs.sendForm('service_2f6la7b', 'template_hz0xt92', '#contact-form')
             .then(function (response) {
                 console.log('SUCCESS!', response.status, response.text);
                 document.getElementById("contact-form").reset();
